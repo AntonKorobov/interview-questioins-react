5a57468daa1137124857802a62782ffcd86ae756
NewAnton <1anton1korobov@gmail.com>
2023-07-30 12:17:11 +0300

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React, {FC} from "react";
+import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 export const App: FC = () => {
   return (
     <div>
       <h1>Hello!</h1>
+      <Link to="about">About Us</Link>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,23 @@
-import React from "react";
-import {createRoot} from "react-dom/client";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
 
-import {App} from "./App";
+import { App } from './App';
 
-createRoot(document.getElementById("root")).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: 'about',
+    element: (
+      <div>
+        <h2>About page</h2>
+        <Link to="/">Main page</Link>
+      </div>
+    )
+  }
+]);
+
+createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
